aacdd6ca66a5c9c421bdc052fb44d3f0f8deca2e
Nevena Kolev <nevena.kolev@htl-wels.at>
2020-10-15 12:36:15 +0200

    implemented interface

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,48 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  * @author Nevena Kolev
  * @version 1.0, 15.10.20
  */
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
+        // launch the JavaFX Application
+        Application.launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Converter
+        VBox root = new VBox();
+
+        //Labels
+        Label lbl_celsius = new Label("Grad Celsius");
+        Label lbl_fahrenheit = new Label("Grad Fahrenheit");
+
+        //Textfields
+        TextField txt_celsiusInput = new TextField();
+        TextField txt_fahrenheitInput = new TextField();
+
+        //Buttons
+        Button btn_convert_CtoF = new Button();
+        Button btn_convert_FtoC = new Button();
+
+        //adding elements
+        root.getChildren().addAll(lbl_celsius, txt_celsiusInput, lbl_fahrenheit, txt_fahrenheitInput, btn_convert_CtoF, btn_convert_FtoC);
+
+        //Scene
+        Scene scn = new Scene(root, 300, 400);
+
+        //Stage
+        Stage stg = new Stage();
+        stg.setTitle("Temp. Converter");
+        stg.setScene(scn);
+        stg.show();
     }
 }
